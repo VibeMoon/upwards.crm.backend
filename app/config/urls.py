692a96bb7f5a51a25e72ede4eb628de81038d3b6
Zulpukarov Marsel <zulpukarovmarsel17@gmail.com>
2025-02-15 16:39:20 +0600

    added swagger

--- app/config/urls.py
+++ app/config/urls.py
@@ -3,12 +3,15 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from .yasg import urlpatterns as docs_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('', include('crm.urls')),
+    path('api-auth/', include('accounts.urls')),
+    path('api/v1/', include('crm.urls')),
 ]
 
+urlpatterns += docs_urls
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
